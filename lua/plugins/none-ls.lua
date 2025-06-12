122ff8dc631aadf5b90ffa2e67673f17fb56e380
Marcos Vinícius Silva <marcoscv71@gmail.com>
2025-06-12 09:09:45 -0300

    config dashboard

--- lua/plugins/none-ls.lua
+++ lua/plugins/none-ls.lua
@@ -8,8 +8,6 @@ return {
 				null_ls.builtins.formatting.stylua,
         null_ls.builtins.formatting.black,
         null_ls.builtins.formatting.isort,
-        null_ls.builtins.formatting.prettier,
-        null_ls.builtins.diagnostics.eslint_d,
 			},
 		})
 
